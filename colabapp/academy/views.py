49c631eeb6436a69584715cbba2c5b0aa3ba9a78
Juan Gutiérrez <ji.gutierrezorellana@gmail.com>
2023-07-11 01:23:54 +0000

    course.html añadido

--- colabapp/academy/views.py
+++ colabapp/academy/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def v_index(request):
     context = {}
     return render(request, 'index.html', context)
+
+def v_course(request):
+    context = {}
+    return render(request, 'course.html', context)
